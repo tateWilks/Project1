d60348823fef5b65e3a8f417c4277d44089f3976
librow <55640527+librow@users.noreply.github.com>
2021-03-16 18:58:12 -0600

    Created the SignupContext model and the SignUpViewModel

---
+++ Models/SignUpContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class SignUpContext : DbContext
+    {
+        // Constructor
+        public SignUpContext (DbContextOptions<SignUpContext> options) : base (options)
+        {
+
+        }
+
+        // Properties
+        public DbSet<SignUpModel> SignUps { get; set; }
+    }
+}
---
+++ Models/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public IEnumerable<SignUpModel> SignUps { get; set; }
+    }
+}
