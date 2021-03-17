2c36ca4d949b77ac1ac2db3d0e6826afe9b3e6b6
librow <55640527+librow@users.noreply.github.com>
2021-03-16 19:06:39 -0600

    Updated appsettings.json and Startup.cs with the ConnectionStrings. Installed the EntityFramework packages (EntityFrameworkCore, .Design, .Sqlite)

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,11 @@ namespace Project1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SignUpContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:Project1Connection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
