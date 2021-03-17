eb5d568b4429d04e52ad8d0da5075761a820dde0
cadensanti5 <70978874+cadensanti5@users.noreply.github.com>
2021-03-17 10:34:45 -0600

    Edited availabletime view to show two separate select options for date and time.

--- Models/SignUpModel.cs
+++ Models/SignUpModel.cs
@@ -4,8 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-//Form names used in SignUp.cshtml: availableTime, groupName, groupSize, email, phone
-
 namespace Project1.Models
 {
     public class SignUpModel
