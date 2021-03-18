adc965f73b6ce65dbddbd78ad1b2a3435e1170a9
librow <55640527+librow@users.noreply.github.com>
2021-03-17 19:28:39 -0600

    Adding PagingInfo ViewModel and updated SignUpViewModel with PagingInfo

---
+++ Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalNumItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        // Creates a total number of pages, rounded up
+        public int TotalPages => (int)(Math.Ceiling((decimal)TotalNumItems / ItemsPerPage));
+    }
+}
