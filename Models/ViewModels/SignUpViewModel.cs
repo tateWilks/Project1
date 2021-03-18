aecb763317d864180c63db12d9d54514bf85731b
librow <55640527+librow@users.noreply.github.com>
2021-03-18 09:57:23 -0600

    Deleted extra code and added some comments in the models

--- Models/ViewModels/SignUpViewModel.cs
+++ Models/ViewModels/SignUpViewModel.cs
@@ -8,12 +8,6 @@ namespace Project1.Models.ViewModels
     public class SignUpViewModel
     {
         public IEnumerable<SignUpModel> SignUps { get; set; }
-        //public AvailableTimes AvailableTimes { get; set; }
         public PagingInfo PagingInfo { get; set; }
-
-        // in the controller, do what you did in the WaterProject
-        // create new ProjectsListViewModel (w/ the linq stuff)
-        // you can get the time passed in from the model
-        // instead of using ViewModel, we could use the viewBag to bring in the chosen timeslot
     }
 }
